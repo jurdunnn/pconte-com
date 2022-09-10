c9a356e8df0473f36f9e6b856dc6947951ce91bf
jurdunnn <jurdun@DESKTOP-2AQ3KRH.localdomain>
2022-09-10 20:33:33 +0100

    Fix fs content block width being too big

--- resources/views/components/fs-content-block.blade.php
+++ resources/views/components/fs-content-block.blade.php
@@ -1,4 +1,4 @@
-<div class="w-screen grid h-[calc(100vh-115.42px)] md:h-[calc(100vh-96px)]  place-items-center">
+<div class="w-full grid h-[calc(100vh-115.42px)] md:h-[calc(100vh-96px)]  place-items-center">
 
     {{ $slot }}
 
