f1a5b96c457e23ebe8c2ae38a344c442ae8cdd3d
jurdunnn <jurdun@DESKTOP-2AQ3KRH.localdomain>
2022-10-19 19:04:34 +0100

    Finalize services block with heroicon

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -1,29 +1,53 @@
 <x-layouts.master>
 
     <x-fs-content-block>
-        <h1>This is a test</h1>
+        <div class="max-w-7xl max-h-2xl w-4/5 mx-auto rounded-2xl text-gray-700 md:-mt-40 p-8 bg-white shadow-md flex flex-col md:flex-row ">
+            <h1 class="font-bold text-2xl w-full p-2 text-center text-gray-800">
+                Emergency Callouts
+                <x-heroicon-s-megaphone class="h-20 mx-auto text-gray-800" />
+            </h1>
+
+            <div class="px-4 pt-2 pb-8 md:px-20 md:pb-12 leading-8">
+                <p>
+                    Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Curabitur aliquet quam id dui posuere blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+                </p>
+            </div>
+
+        </div>
+
     </x-fs-content-block>
 
     <x-fs-content-block>
-        <h1>This is a test</h1>
+        <div class="max-w-7xl max-h-2xl w-4/5 mx-auto rounded-2xl text-gray-700 md:-mt-40 p-8 bg-white shadow-md flex flex-col md:flex-row ">
+            <div class="px-4 pt-2 pb-8 md:px-20 md:pb-12 leading-8">
+                <p>
+                    Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Curabitur aliquet quam id dui posuere blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+                </p>
+            </div>
+
+            <h1 class="font-bold text-2xl w-full p-2 text-center text-gray-800">
+                Emergency Callouts
+                <x-heroicon-s-megaphone class="h-20 mx-auto text-gray-800" />
+            </h1>
+        </div>
+
     </x-fs-content-block>
 
     <x-fs-content-block>
-        <h1>This is a test</h1>
-    </x-fs-content-block>
-
-    <x-fs-content-block>
-        <div
-            class="embed-responsive border-0 rounded-xl embed-responsive-21by9 relative w-2/3 overflow-hidden"
-            style="padding-top: 42.857143%"
-            >
-            <iframe
-                class="embed-responsive-item absolute top-0 right-0 bottom-0 left-0 w-full h-full"
-                src="https://www.youtube.com/embed/vlDzYIIOYmM?enablejsapi=1&amp;origin=https%3A%2F%2Fmdbootstrap.com"
-                allowfullscreen=""
-                data-gtm-yt-inspected-2340190_699="true"
-                id="240632615"
-                ></iframe>
+        <div class="w-full -mt-[18rem]">
+            <div class="bg-gradient-to-br from-pink-400 to-yellow-300 dark:from-pink-500 dark:to-yellow-500 text-white mx-auto max-w-7xl w-4/5 py-8 px-4 border-0 rounded-xl shadow sm:px-6 lg:py-10 lg:px-8">
+                <div class="mx-auto max-w-3xl flex flex-col gap-y-6 text-center text-2xl">
+                    <h1 class="font-bold">Emergency Callouts</h1>
+                    <h1>Pellentesque in ipsum id orci porta dapibus. Nulla quis lorem ut libero malesuada feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Curabitur aliquet quam id dui posuere blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.
+                        <br><br>
+                        Donec rutrum congue leo eget malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Nulla porttitor accumsan tincidunt. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Donec sollicitudin molestie malesuada.</h1>
+                </div>
+            </div>
         </div>
     </x-fs-content-block>
+
+    <x-fs-content-block>
+        <h1>This is a test</h1>
+    </x-fs-content-block>
+
 </x-layouts.master>
