acc3c1509a09ae82c063f4f9be2d0372d7b36576
Jurdunnn <jordy.downs@hotmail.co.uk>
2023-01-15 14:10:40 +0000

    Add different length personal profiles based on screen size

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,16 +5,15 @@
 
 @section('content')
     <div class="absolute flex flex-col text-5xl text-white top-4 md:top-1/2 left-1/2 gap-y-6 -translate-x-1/2 md:-translate-y-[19rem]">
-        <div class="relative ml-auto mr-auto w-24 h-24">
-            <x-heroicon-o-sun class="w-full h-full" />
-        </div>
 
-        <h1 class="font-semibold ml-auto mr-auto text-center">Conte's Domestic Electrical Care</h1>
+        <h1 class="font-semibold ml-auto mr-auto mt-24 text-center">Conte's Domestic Electrical Care</h1>
 
         <div class="hidden lg:block">
             <x-divider />
         </div>
 
+
+        <!--Laptop and up -->
         <p class="font-light mt-4 leading-8 text-center hidden lg:block text-lg lg:text-xl">
             With over 45 years of experience, I am a highly skilled and knowledgeable professional who is dedicated to
             providing the highest level of service to my clients. I am well-versed in all aspects of domestic electrical 
@@ -24,9 +23,24 @@
             completely satisfied with the work that I do.
         </p>
 
+        <!--Tablet and up -->
+        <div class="hidden sm:block">
+            <p class="font-light mt-4 leading-8 text-center block lg:hidden text-lg">
+                I am an experienced electrician with over 45 years of experience in domestic work. Skilled in all aspects of
+                wiring, lighting, and appliance installation and repair. I pride myself in providing high-quality service and
+                ensuring customer satisfaction. My expertise allows me to tackle any project with confidence and skill.        
+            </p>
+        </div>
+
+        <!--Mobile and up -->
+        <p class="font-light mt-2 leading-6 text-center block sm:hidden text-lg">
+            Experienced electrician with 45 years of domestic work experience. Skilled in all areas, committed
+            to quality service.
+        </p>
+
         <a
             href="{{ route('contact') }}"
-            class="inline-flex items-center mt-12 ml-auto mr-auto rounded-md border border-transparent bg-yellow-600 px-12 py-3 text-base font-medium text-white shadow-sm hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2"
+            class="inline-flex items-center mt-4 xl:mt-12 ml-auto mr-auto rounded-md border border-transparent bg-yellow-600 px-12 py-3 text-base font-medium text-white shadow-sm hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2"
             >
             Get a quote!
         </a>
