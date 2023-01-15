319c2063301a3acd8c3412d279c1cdbac64e222c
Jurdunnn <jordy.downs@hotmail.co.uk>
2023-01-15 01:48:50 +0000

    Add home link to nav links for smaller breakpoints

--- resources/views/components/nav-links.blade.php
+++ resources/views/components/nav-links.blade.php
@@ -1,4 +1,7 @@
 <ul class="navbar-nav ml-auto flex flex-col font-semibold pl-0 list-style-none">
+    <li class="lg:invisible nav-item py-9 px-4 @if (Route::is('services')) bg-black bg-opacity-25 text-yellow-500 @else text-white hover:text-yellow-500 @endif">
+        <a class="nav-link" href="{{ route('index') }}">Home</a>
+    </li>
     <li class="nav-item py-9 px-4 @if (Route::is('services')) bg-black bg-opacity-25 text-yellow-500 @else text-white hover:text-yellow-500 @endif">
         <a class="nav-link" href="{{ route('services') }}">Services</a>
     </li>
