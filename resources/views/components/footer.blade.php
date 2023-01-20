97558968d7626e5520912597b8ff3b5b9957cc1a
Jurdunnn <jordy.downs@hotmail.co.uk>
2023-01-20 21:35:08 +0000

    Add repo url to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -93,7 +93,7 @@
             </a>
 
 
-            <a href="#!" type="button" class="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+            <a href="https://github.com/jurdunnn/pconte-com" target="_blank" type="button" class="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
                 <svg aria-hidden="true"
                      focusable="false"
                      data-prefix="fab"
