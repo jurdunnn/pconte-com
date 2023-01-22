ab0f9183b09a5b8d594651d01928a054c6d39938
Jurdunnn <jordy.downs@hotmail.co.uk>
2023-01-22 16:47:05 +0000

    Add color accents to services page

--- resources/views/components/services-block.blade.php
+++ resources/views/components/services-block.blade.php
@@ -1,7 +1,7 @@
 @props(['title' => null,])
 
 <div class="p-4 text-gray-600 dark:text-gray-200 py-12 select-none">
-    <h1 class="mb-8 text-center mx-auto text-3xl font-bold">{{ $title }}</h1>
+    <h1 class="mb-8 text-center mx-auto text-3xl text-yellow-500 font-bold">{{ $title }}</h1>
 
     <ul class="grid place-content-center grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {{ $slot }}
--- resources/views/components/services-item.blade.php
+++ resources/views/components/services-item.blade.php
@@ -1,7 +1,7 @@
 @props(['number' => null, 'title' => null,])
 
 <li class="flex">
-    <div class="px-4 text-5xl font-extralight">{{ $number }}</div>
+    <div class="px-4 text-5xl text-yellow-500 font-extralight">{{ $number }}</div>
     <div>
         <div class="text-xl font-bold">{{ $title }}</div>
         <p class="max-w-xs py-2 text-sm">{{ $slot }}</p>
